803f21bb36336e35c4ab41a6b20886fa516a81c6
Micael Picco <micaelpicco@MacBook-Pro-de-Micael.local>
2022-12-16 17:35:55 -0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Store } from "./pages/Store";
-import { Navbar } from "./components/NavBar";
+import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App() {
